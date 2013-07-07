e06925732d85c88da1d410f9d0c28b604e17fb0b
Nikolay <ypypy28@gmail.com>
2013-07-08 01:10:06 +0400

    lesson 4 complete

--- lesson4/src/com/lesson/Human.java
+++ lesson4/src/com/lesson/Human.java
@@ -1,13 +1,51 @@
 package com.lesson;
 
+/*
+    public
+    protected
+    – default "package locale"
+    private
+ */
+
 public class Human {
 
-    public String name;
+    private final int minAge = 1; // final => const
 
-    public void getName() {
+    private final int maxAge = 120;
+
+    private final int minNameLength = 3;
+
+    private String name = "N/A";
+
+    private int age;
+
+    public int getAge(){
+        return age;
+    }
+
+    public void setAge(int newAge) {
+        if ( newAge >= minAge && newAge < maxAge) {
+            age = newAge;
+        } else {
+            System.out.println("age setting ERROR!!11!");
+        }
+    }
+
+    public String getName() {
         return name;
 
     }
+
+    public void sayHello() {
+        System.out.println("Hi!");
+    }
+
+    public void setName(String newName) {
+        if ( newName != null && newName.length() >= minNameLength ) {
+            name = newName;
+        }
+    }
+
 
 
 }
--- lesson4/src/com/lesson/Main.java
+++ lesson4/src/com/lesson/Main.java
@@ -4,7 +4,26 @@ public class Main {
 
        public static void main( String[] args ){
            Human human = new Human();
-           human.getName();
+           human.setName("Max");
+           System.out.println(human.getName());
+
+           human.setAge(22);
+           System.out.println(human.getAge());
+
+           human.setAge(-3);
+           System.out.println(human.getAge());
+           System.out.println("~~~~~~~~~~~");
+           human.setAge(128);
+           System.out.println(human.getAge());
+
+
+           human.setName("Kolya");
+           System.out.println(human.getName());
+           human.setName(null);
+           System.out.println(human.getName());
+           human.setName("bu");
+           System.out.println(human.getName());
+
        }
 
 }
