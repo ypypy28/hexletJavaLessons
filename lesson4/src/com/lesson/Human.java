ddcdf9278b07b7a7f41b4e35526de7bc25b2db72
Nikolay <ypypy28@gmail.com>
2013-07-07 22:56:06 +0400

    just started a 4th lesson

---
+++ lesson4/src/com/lesson/Human.java
@@ -0,0 +1,13 @@
+package com.lesson;
+
+public class Human {
+
+    public String name;
+
+    public void getName() {
+        return name;
+
+    }
+
+
+}
---
+++ lesson4/src/com/lesson/Main.java
@@ -0,0 +1,10 @@
+package com.lesson;
+
+public class Main {
+
+       public static void main( String[] args ){
+           Human human = new Human();
+           human.getName();
+       }
+
+}
