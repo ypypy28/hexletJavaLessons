d779fe174eae8c466c8bd59da6e8cfaea852c207
Nikolay Dyachkov <ypypy28@yandex.ru>
2013-07-15 00:19:15 +0400

    finished lesson5

--- lesson5/src/com/lesson/Field.java
+++ lesson5/src/com/lesson/Field.java
@@ -2,9 +2,40 @@ package com.lesson;
 
 public class Field {
 
-    private char[] field = new char[3];
+    private static final int FIELD_SIZE = 3;
+
+    private static final char DEFAULT_FIELD_VALUE = ' ';
+
+    private char[][] field = new char[FIELD_SIZE][FIELD_SIZE];
+
+    private void eraseLine( int lineNumber ) {
+        for ( int i = 0; i < FIELD_SIZE; i++ ) {
+            field[i][lineNumber] = DEFAULT_FIELD_VALUE;
+        }
+    }
+
+    private void showCell( int x, int y ) {
+        System.out.print( "[" + field[x][y] + "]" );
+    }
+
+    private void showLine( int lineNumber ) {
+        for ( int i = 0; i < FIELD_SIZE; i++ ) {
+            showCell( i, lineNumber );
+        }
+    }
+
+    public void eraseField() {
+        for ( int i = 0; i < FIELD_SIZE; i++ ) {
+            eraseLine(i);
+        }
+    }
 
     public void showField() {
-        System.out.print(field[0]);
+        for ( int i = 0; i < FIELD_SIZE; i++ ) {
+            showLine(i);
+            System.out.println();
+        }
+        System.out.println();
     }
+
 }
--- lesson5/src/com/lesson/Main.java
+++ lesson5/src/com/lesson/Main.java
@@ -3,33 +3,12 @@ package com.lesson;
 public class Main {
 
     public static void main(String[] args) {
-        ExampleStaticFieldClass exampleStaticFieldClass = new ExampleStaticFieldClass();
-        ExampleStaticFieldClass exampleStaticFieldClass2 = new ExampleStaticFieldClass();
-
-        exampleStaticFieldClass.nonStaticField = 4;
-        ExampleStaticFieldClass.staticField = 4;  // - так правильно т.к. static - class level variable (а не instance level)
-
-        exampleStaticFieldClass2.nonStaticField = 5;
-        ExampleStaticFieldClass.staticField = 5;
-
-        System.out.println(exampleStaticFieldClass.nonStaticField + ", " + exampleStaticFieldClass2.nonStaticField);
-        System.out.println(ExampleStaticFieldClass.staticField + ", " + ExampleStaticFieldClass.staticField);
-
-        System.out.println("Human.Min_AGE: " + Human.MIN_AGE);
-
-        exampleStaticFieldClass.hello();
-        ExampleStaticFieldClass.helloStatic();
+        Field field = new Field();
+        field.eraseField();
+        field.showField();
     }
 }
 
 /*
-    ExampleStaticFieldClass
-        staticField    = 5
-
-    exampleStaticFieldClass
-        nonStaticField = 4
-
-    exampleStaticFieldClass2
-        nonStaticField = 5
 
 */
